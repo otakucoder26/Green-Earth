59d7ceeb3a7904a29ecb188b707261366675b059
otakucoder26 <khanrahmatullah2606@gmail.com>
2025-09-09 22:46:53 +0600

    Last commit

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const clickBtn = (id) => {
     .then((json) => {
       removeActive();
       const clickBtn = document.getElementById(`category-btn-${id}`);
-      // console.log(clickBtn)
+      
       clickBtn.classList.add("active");
       displayCategories(json.plants);
     });
@@ -71,7 +71,7 @@ const displayCategories = (plants) => {
   const plantsCard = document.getElementById("allCards");
   plantsCard.innerHTML = "";
   plants.forEach((plant) => {
-    // console.log(plant);
+   
     const div = document.createElement("div");
     div.innerHTML = `
         <div class="bg-white p-4 rounded-lg">
@@ -84,11 +84,11 @@ const displayCategories = (plants) => {
                     <p class="opacity-70 line-clamp-2">${plant.description}</p>
                  </div>
                  <div class="flex justify-between items-center py-2">
-                    <button class="btn border-none bg-[#DCFCE7] text-[#15803D] font-semibold rounded-3xl"
-          >
-            ${plant.category}
-          </button>
-          <p class="font-bold">৳ <span>${plant.price}</span></p>
+                    <button class="btn border-none bg-[#DCFCE7] text-[#15803D] font-semibold rounded-3xl">
+          
+                     ${plant.category}
+                   </button>
+                   <p class="font-bold">৳ <span>${plant.price}</span></p>
                  </div>
                  <button onclick="lodePrice('${plant.name}', ${plant.price})" class="btn bg-[#15803D] text-white rounded-3xl w-full">Add to Cart</button>
             </div>
@@ -103,7 +103,7 @@ const displayAllTrees = (trees) => {
   const treesCard = document.getElementById("categotis");
   treesCard.innerHTML = "";
   trees.forEach((tree) => {
-    // console.log(tree)
+   
     const div = document.createElement("div");
     div.innerHTML = `
         <button id="category-btn-${tree.id}" onclick="clickBtn(${tree.id})" class="text-left p-2 font-semibold w-full rounded-sm hover:bg-[#85ffb6] categoriesBtn">${tree.category_name}</button>
@@ -112,3 +112,87 @@ const displayAllTrees = (trees) => {
   });
 };
 allTrees();
+
+// ALlCards
+const allCards = () => {
+  manageSpinner(true);
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then((res) => res.json())
+    .then((json) => displayAllCards(json.plants));
+};
+// AllCards Full
+const displayAllCards = (plants) => {
+  const plantsCard = document.getElementById("allCards");
+  plantsCard.innerHTML = "";
+  plants.forEach((plant) => {
+    
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="bg-white p-4 rounded-lg">
+                 <div>
+                    <img src="${plant.image}" class="rounded-lg h-[168px] w-[350px] object-cover mx-auto" alt="">
+                 </div>
+                 <div class="space-y-2 mt-2">
+                 <button class="text-xl font-bold" onclick="lodeTreeDetail(${plant.id})">${plant.name}</button>
+                    
+                    <p class="opacity-70 line-clamp-2">${plant.description}</p>
+                 </div>
+                 <div class="flex justify-between items-center py-2">
+                   
+                 
+                  <button  class="btn border-none bg-[#DCFCE7] text-[#15803D] font-semibold rounded-3xl">
+          
+                    ${plant.category}
+                  </button>
+                 <p class="font-bold">৳ <span>${plant.price}</span></p>
+                 </div>
+                 <button onclick="lodePrice('${plant.name}', ${plant.price})" class="btn bg-[#15803D] text-white rounded-3xl w-full">Add to Cart</button>
+            </div>
+        `;
+    plantsCard.appendChild(div);
+  });
+  manageSpinner(false);
+};
+const allTreesBtn = () => {
+  removeActive();
+  allCards();
+  const categoriesBtn = document.getElementById("categoriesBtn");
+  categoriesBtn.classList.add("active");
+};
+
+// TOTAL CALCULATE PRICE
+let total = 0;
+const lodePrice = (name, price) => {
+  const yourCard = document.getElementById("yourCard");
+  const spanTotal = document.getElementById("spnTotal");
+  const div = document.createElement("div");
+  alert("Add To Cart");
+  div.innerHTML = `
+    <div
+              class="flex justify-between items-center bg-[#DCFCE7] p-2 rounded-lg">
+            
+              <div class="space-y-3">
+                <h2 class="font-bold">${name}</h2>
+                <p class="opacity-70">৳ <span id="itemPrice"> ${price}</span> × 1</p>
+              </div>
+              <div class="opacity-70 text-red-500">
+                <i class="fa-solid fa-xmark"></i>
+              </div>
+            </div>
+    `;
+  // TOTAL ADD PRICE
+
+
+  total += price;
+  spanTotal.innerText = total;
+  div.querySelector("i").addEventListener("click", function () {
+    div.remove();
+   
+    total -= price;
+    spanTotal.innerText = total;
+  });
+  yourCard.appendChild(div);
+
+};
+
+allCards();
